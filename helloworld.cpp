3c8017a3f0c71c6ed56bc72482f9856d3c11bd00
Richard Hayes <brawl1336@gmail.com>
2022-09-20 16:38:16 -0700

    updated helloworld.cpp to read out README.md

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include <fstream>
+#include<string>
 
+//we want to read in readme.md and print its contents to console
 int main ()
 {
 
-	std::cout << "Hello" << std::endl;
+	std::cout << "Hello, we will now read README.md" << std::endl << std::endl;
+
+
+	std::string printer;
+	
+	std::ifstream reader;
+	reader.open("README.md", std::ios::in);
+	//open README.md for future operations
+	
+	
+	while ( !reader.eof() )
+	{
+		reader >> printer;
+		std::cout << printer << std::endl;
+	}
+	reader.close();
+	
   	return 0;
 }
 
